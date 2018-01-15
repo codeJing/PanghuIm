0470942a2bffe81165a25301f96c2c214904b9bc
codejing <icanflyjing@163.com>
2018-01-15 23:14:37 +0800

    mainActivity
    1次修改

---
+++ app/src/main/java/com/jing/panghuim/activities/MainActivity.java
@@ -0,0 +1,23 @@
+package com.jing.panghuim.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jing.common.app.BaseActivity;
+import com.jing.panghuim.R;
+
+public class MainActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    protected int getContentLayoutId() {
+        return R.layout.activity_main;
+    }
+}
